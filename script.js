8f610317a632d4702132e1be463c4efd5d9df0e6
.!Kattepus2004!. <thomas.bolli@outlook.com>
2024-11-19 03:02:32 +0100

    Design Rework stage.1

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ const yearSpan = document.getElementById("year-date");
 yearSpan.innerText = new Date().getFullYear();
 
 
+const navbar = document.querySelector('.navbar');
+const body = document.getElementsByTagName('body')[0]
+
+window.addEventListener('scroll', () => {
+    // Calculate degrees based on scroll position
+    const scrollY = window.scrollY;
+    // Optional: Change navbar class based on scroll position
+    if (scrollY > 200.5) { 
+      navbar.classList.add('shrink');
+    } else {
+      navbar.classList.remove('shrink');
+    }
+  });
+
